90b0bc417e6441939c1fd764550fdd0d498cbb07
John Nolcox <Jnolcox0429+github@gmail.com>
2017-05-18 13:50:44 -0400

    Update Playoffs.java
    added user entry

--- Recursion/Playoffs.java
+++ Recursion/Playoffs.java
@@ -3,28 +3,43 @@
   to the playoffs using recursion.
 */
 
+import java.util.Scanner;
+
 public class Playoffs {
 	public static void main(String[] args) {
-		int points = 0;
-		String record = "";
-		int game = 0;
+//		int points = 0;
+//		String record = "";
+//		int game = 0;
+//		
+//		while (game < 5) {
+//			points +=3;
+//			record +="W";
+//			game ++;
+//		}
+//		System.out.println(record + " " + points);
 		
-		while (game < 5) {
-			points +=3;
-			record +="W";
-			game ++;
-		}
-		//System.out.println(record + " " + points);
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
 		
-		games(0, " ", 0);
+		System.out.println("Enter the number of games to be played: \r");
+		int games = input.nextInt();
+		
+		System.out.println("Enter the points required to progress: \r");
+		int cutoff = input.nextInt();
+		
+		
+		games(0, 0, " ", games, cutoff);
 	}
 	
-	public static void games(int p, String r, int g) {
-		if(g < 5) {
-			games( p+3, r+"W", g+1 );
-			games( p+1, r+"T", g+1 );
-			games( p, r+"L", g+1 );
-		} else if (p > 9 ) {
+	
+	
+	public static void games(int p, int i, String r, int g, int entry) {
+		
+		if(i < g) {
+			games( p+3, i+1, r+"W", g, entry);
+			games( p+1, i+1, r+"T", g, entry);
+			games( p, i+1, r+"L", g, entry);
+		} else if (p > entry ) {
 			System.out.println( r + " " + p);
 		}
 	}
